a31e72d141142db22c4522055720c23138dd314a
Torin Miner <torin.miner@gmail.com>
2021-10-12 14:29:13 -0500

    added watchlist to admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Listing, Bid, Comment, Category
+from .models import User, Listing, Bid, Comment, Category, Watchlist
 
 # Register your models here.
 class ListingAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid)
 admin.site.register(Comment)
 admin.site.register(Category)
+admin.site.register(Watchlist)
